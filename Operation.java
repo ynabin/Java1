d45563e9cde3492273ab4e1542f1b5a5aa3e1782
Nabin Yadav <110272675+ynabin@users.noreply.github.com>
2024-05-17 11:18:47 -0700

    Add files via upload
    Simple Program of Java

---
+++ Operation.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.wrapperclass;
+
+/**
+ *
+ * @author Nabin Yaduvanshi
+ */
+public class Operation {
+    int data=50;
+    
+    void change(int data){
+        data=data+100;//changes will be in the local variable only
+    }
+    
+    
+}
